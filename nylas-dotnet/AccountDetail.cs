58aca73fbc5d4f1f0c1c288bc6c140d99bfbb231
Nick Oliver <nickoliver@affinitycanvas.com>
2023-04-01 14:49:53 -0500

    Adds NylasAccount class representing a user account with methods to revoke an access token and get the account details.

---
+++ nylas-dotnet/AccountDetail.cs
@@ -0,0 +1,19 @@
+﻿namespace nylas_dotnet
+{
+    public class AccountDetail
+    {
+        public string? id { get; set; }
+        public string? account_id { get; set; }
+        public string? name { get; set; }
+        public string? email_address { get; set; }
+        public string? provider { get; set; }
+        public string? organization_unit { get; set; }
+        public string? sync_state { get; set; }
+        public long? linked_at { get; set; }
+
+        public override string ToString()
+        {
+            return $"AccountDetail [id={id}, account_id={account_id}, name={name}, email_address={email_address}, provider={provider}, organization_unit={organization_unit}, sync_state={sync_state}, linked_at={linked_at}]";
+        }
+    }
+}
